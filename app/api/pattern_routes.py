1a7752d63bf633f820ab4060c2621991f188f788
Taylor <taylorashley1304@gmail.com>
2024-08-22 21:17:51 -0400

    get all routes

--- app/api/pattern_routes.py
+++ app/api/pattern_routes.py
@@ -2,10 +2,11 @@ from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
 from app.models import Pattern, db
 
-pattern_routes = Blueprint('patterns', __name__, url_prefix="/patterns")
+pattern_routes = Blueprint('patterns', __name__)
 
 #get all patterns (limit 25?)
-@pattern_routes.route('')
+@pattern_routes.route('/')
 def all_patterns():
+    print('Hello World Check')
     patterns=Pattern.query.all()
-    return jsonify({'patterns' : [pattern.to_dict() for pattern in patterns]})
+    return {'patterns' : [pattern.to_dict() for pattern in patterns]}
