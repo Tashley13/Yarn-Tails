74a531ae1fd117c54e0845e8555f42d213cf3aa8
Taylor <taylorashley1304@gmail.com>
2024-08-28 20:38:45 -0400

    successfully displaying information for AllPatterns

--- react-vite/src/components/AllPatterns/AllPatterns.jsx
+++ react-vite/src/components/AllPatterns/AllPatterns.jsx
@@ -11,17 +11,45 @@ const AllPatterns = () => {
     //if user clicks a pattern tile, check if they are logged in
     const loggedIn= useSelector((state)=>state.session.user)
 
+
     //grab all the patterns
-    const patterns=useSelector((state) => state.patterns.allPatterns);
-    console.log("PATTERNS: ", patterns)
+    const eachPattern=useSelector((state) => state.patterns.allPatterns);
+    const patterns=Object.values(eachPattern)[0]
+    // console.log("PATTERNS: ", patterns.length)
 
     useEffect(()=> {
         dispatch(patternActions.getAllPatterns())
-    }, )
+    }, [dispatch])
+
+    const createPattern = async () => {
+        //navigate to create a pattern page
+    }
+
+    if (!patterns) {
+        //insert button that says 'create a pattern', use createPattern
+        return <div>No Patterns Yet!</div>
+    }
 
     return (
-        <div>
-            Hello World
+        <div className="patterns-display">
+           <ul>
+            {patterns?.length > 0 ? (
+                patterns.map((pattern, key) => (
+                    <div key={key} className="pattern-display">
+                        <div className="pattern-title">
+                            {pattern.title}
+                        </div>
+                        <div className="pattern-image">
+                            {pattern.tile_image}
+                        </div>
+                        <div className="pattern-difficulty">
+                            {pattern.difficulty}
+                        </div>
+
+                    </div>
+                ))
+            ): ''}
+           </ul>
         </div>
     )
 }
