275ec0422408c677a7fc7985fd493981ca8928bf
Taylor <taylorashley1304@gmail.com>
2024-09-14 20:11:26 -0400

    getting 2 feature ready

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,4 @@ from .user import User
 from .db import environment, SCHEMA
 from .pattern import Pattern
 from .tester import Tester
-from .pattern_image import PatternImage
+# from .pattern_image import PatternImage
--- app/models/pattern_image.py
+++ app/models/pattern_image.py
@@ -1,32 +1,32 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
-from datetime import datetime
-from sqlalchemy import JSON
+# from .db import db, environment, SCHEMA, add_prefix_for_prod
+# from datetime import datetime
+# from sqlalchemy import JSON
 
-class PatternImage(db.Model):
-    __tablename__='pattern_images'
+# class PatternImage(db.Model):
+#     __tablename__='pattern_images'
 
-    if environment == "production":
-        __table_args__={"schema": SCHEMA}
+#     if environment == "production":
+#         __table_args__={"schema": SCHEMA}
 
-    id=db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='CASCADE'), nullable=False)
-    pattern_id=db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('patterns.id'), ondelete='CASCADE'), nullable=False)
-    image=db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now)
-    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+#     id=db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='CASCADE'), nullable=False)
+#     pattern_id=db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('patterns.id'), ondelete='CASCADE'), nullable=False)
+#     image=db.Column(db.String, nullable=False)
+#     created_at = db.Column(db.DateTime, default=datetime.now)
+#     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
-    pattern= db.relationship('Pattern', back_populates='pattern_images')
-    user = db.relationship('User', back_populates='pattern_images')
+#     pattern= db.relationship('Pattern', back_populates='pattern_images')
+#     user = db.relationship('User', back_populates='pattern_images')
 
-    def __repr__(self):
-        return f'PatternImage {self.id}'
+#     def __repr__(self):
+#         return f'PatternImage {self.id}'
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'pattern_id': self.pattern_id,
-            'image': self.image,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at
-        }
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'user_id': self.user_id,
+#             'pattern_id': self.pattern_id,
+#             'image': self.image,
+#             'created_at': self.created_at,
+#             'updated_at': self.updated_at
+#         }
